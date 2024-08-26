4fc8ed50f861dc46c02cfd14b0d12565d0e61f6c
ajinkode <ajinkode@gmail.com>
2024-08-26 12:02:48 +0530

    Change port to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { connectToDatabase, queryTask2, queryTask3 } = require('./db');
 const path = require('path');
 
 const app = express();
-const port = 443;
+const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
