fbc406fa98f74b6a406a171250e9c0b1f41cb6fa
ajinkode <ajinkode@gmail.com>
2024-08-26 11:02:13 +0530

    Change port to 443

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { connectToDatabase, queryTask2, queryTask3 } = require('./db');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
