4e4a36520b3a98b2ddec1554c297b4692555fc31
ajinkode <ajinkode@gmail.com>
2024-08-26 12:24:14 +0530

    Chnage default port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { connectToDatabase, queryTask2, queryTask3 } = require('./db');
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
